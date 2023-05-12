e3b0914379988140ab5ccc5585d5c3eca9796d53
tobyarnett <39145409+tobyarnett@users.noreply.github.com>
2023-05-11 22:37:41 -0500

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "[i] Updating and upgrading Kali (this will take a while)"
-sudo apt-get update -yes
-sudo apt-get dist-upgrade -yes
+sudo apt-get update -y
+sudo apt-get dist-upgrade -y
 
 echo "[i] Installing Xfce4 & xrdp (this will take a while as well)"
-sudo apt-get install kali-desktop-xfce xorg xrdp -yes
+sudo apt-get install kali-desktop-xfce xorg xrdp -y
 
 echo "[i] Configuring xrdp to listen to port 3390 (but not starting the service)"
 sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
